8e776ce76523e1f9214e20b4ae11614201194552
sahand mahdilu <sahand.mahdilu29@gmail.com>
2024-08-06 18:40:58 +0330

    quiz app added to git hub

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+import MainTodo from './mainTodo/MainTodo'
+
+function App() {
+ 
+
+  return <MainTodo/>
+}
+
+export default App
---
+++ src/mainTodo/MainTodo.jsx
@@ -0,0 +1,14 @@
+
+
+export default function MainTodo() {
+  return (
+    <div>
+      <h1> Todo list</h1>
+<div className="flex flex-col justify-center items-center">
+<input type="text" placeholder="enter todo" className=""/>
+<button className="bg-blue-500 w-[300px] rounded-lg">ADD</button>
+</div>
+
+    </div>
+  )
+}
